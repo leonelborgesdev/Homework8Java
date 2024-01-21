1245c3815079c3d77a957847928b24b90a94b186
leonelborgesdev <leonel03t@gmail.com>
2024-01-21 00:10:02 -0400

    EmployeeDaoImpl methods

--- src/dao/impl/EmployeeDaoImpl.java
+++ src/dao/impl/EmployeeDaoImpl.java
@@ -1,4 +1,29 @@
 package dao.impl;
 
-public class EmployeeDaoImpl {
+import dao.EmployeeDao;
+import dao.dto.EmployeeDto;
+import domain.Employee;
+
+import java.util.List;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+    @Override
+    public void insertEmployee(EmployeeDto employee) {
+
+    }
+
+    @Override
+    public void updateEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        return null;
+    }
 }
