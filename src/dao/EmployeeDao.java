c4b7c20508df8d6bf160748a727eeb7e1b48d070
leonelborgesdev <leonel03t@gmail.com>
2024-01-21 00:05:37 -0400

    interface emplyeeDao

--- src/dao/EmployeeDao.java
+++ src/dao/EmployeeDao.java
@@ -1,7 +1,13 @@
 package dao;
 
+import dao.dto.EmployeeDto;
 import domain.Employee;
 
+import java.util.List;
+
 public interface EmployeeDao {
-    void insertEmployee(Employee)
+    void insertEmployee(EmployeeDto employee);
+    void updateEmployee(Employee employee);
+    void deleteEmployee(int id);
+    List<EmployeeDto> getAllEmployees();
 }
