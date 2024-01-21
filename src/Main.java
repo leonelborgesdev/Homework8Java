6465600d9a887d1d1032d0926a56bda470c3b418
leonelborgesdev <leonel03t@gmail.com>
2024-01-21 00:22:09 -0400

    mostrando lista de empleados

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import dao.dto.EmployeeDto;
 import dao.impl.EmployeeDaoImpl;
 
 import java.sql.SQLException;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -15,5 +16,10 @@ public class Main {
         //Insertar empleado
         EmployeeDto emp1=new EmployeeDto("John Doe", 50000.0);
         dao.insertEmployee(emp1);
+
+        List<EmployeeDto> employeeDtos= dao.getAllEmployees();
+        for (EmployeeDto employee : employeeDtos){
+            System.out.println("-> "+ employee.getName() + " | "+ employee.getSalary());
+        }
     }
 }
