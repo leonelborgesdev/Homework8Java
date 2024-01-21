85cf61c3756777eb6027d74fef489836256ad63d
leonelborgesdev <leonel03t@gmail.com>
2024-01-21 00:11:14 -0400

    EmployeeDaoImpl consultas

--- src/dao/impl/EmployeeDaoImpl.java
+++ src/dao/impl/EmployeeDaoImpl.java
@@ -7,6 +7,11 @@ import domain.Employee;
 import java.util.List;
 
 public class EmployeeDaoImpl implements EmployeeDao {
+    private static final String INSERT_INTO_EMPLOYEES = "INSERT INTO employees (name, salary) VALUES (?, ?)";
+    private static final String UPDATE_EMPLOYEES = "UPDATE employees SET name=?, salary=? WHERE id=?";
+    private static final String DELETE_FROM_EMPLOYEES = "DELETE FROM employees WHERE id=?";
+    private static final String SELECT_FROM_EMPLOYEES = "SELECT * FROM employees";
+
     @Override
     public void insertEmployee(EmployeeDto employee) {
 
