4db6904ac0a7b084ab332ee7af3870026d93409d
leonelborgesdev <leonel03t@gmail.com>
2024-01-20 23:50:43 -0400

    getconnection in jdbcconfig

--- src/config/JdbcConfig.java
+++ src/config/JdbcConfig.java
@@ -1,6 +1,7 @@
 package config;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConfig {
@@ -8,6 +9,11 @@ public class JdbcConfig {
     private static final String JDBC_USER= "sa";
     private static final String JDBC_PASSWORD= "";
     public static Connection getConnection() throws SQLException{
-        return null;
+       try{
+           Class.forName("org.h2.Driver");
+           return DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD);
+       }catch (ClassNotFoundException e){
+           throw  new RuntimeException(e);
+       }
     }
 }
