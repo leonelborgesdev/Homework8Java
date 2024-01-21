cafe252ac8cdc93e33c454c02d8e21a0da7d8373
leonelborgesdev <leonel03t@gmail.com>
2024-01-20 23:59:57 -0400

    employee pojo

--- src/domain/Employee.java
+++ src/domain/Employee.java
@@ -1,4 +1,31 @@
 package domain;
 
 public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
 }
