55feb287cce47fe9caa023651fa6550bd50cb9e6
leonelborgesdev <leonel03t@gmail.com>
2024-01-20 23:48:19 -0400

    connection get connection

--- src/config/JdbcConfig.java
+++ src/config/JdbcConfig.java
@@ -1,4 +1,13 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class JdbcConfig {
+    private static final String JDBC_URL = "jdbc:h2:~/employees";
+    private static final String JDBC_USER= "sa";
+    private static final String JDBC_PASSWORD= "";
+    public static Connection getConnection() throws SQLException{
+        return null;
+    }
 }
