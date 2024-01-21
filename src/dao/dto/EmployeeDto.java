0e34224c67188eea06ede7e9698eb64359316a83
leonelborgesdev <leonel03t@gmail.com>
2024-01-21 00:03:40 -0400

    pojo EmployeeDto

--- src/dao/dto/EmployeeDto.java
+++ src/dao/dto/EmployeeDto.java
@@ -1,4 +1,16 @@
 package dao.dto;
 
 public class EmployeeDto {
+    private String name;
+    private double salary;
+    public EmployeeDto(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getSalary(){
+        return salary;
+    }
 }
