e2f392ddcc1299318fb89b5f41a8b7a798f853b5
leonelborgesdev <leonel03t@gmail.com>
2024-01-21 00:13:43 -0400

    insertEmployee

--- src/dao/impl/EmployeeDaoImpl.java
+++ src/dao/impl/EmployeeDaoImpl.java
@@ -1,9 +1,13 @@
 package dao.impl;
 
+import config.JdbcConfig;
 import dao.EmployeeDao;
 import dao.dto.EmployeeDto;
 import domain.Employee;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeeDaoImpl implements EmployeeDao {
@@ -13,8 +17,25 @@ public class EmployeeDaoImpl implements EmployeeDao {
     private static final String SELECT_FROM_EMPLOYEES = "SELECT * FROM employees";
 
     @Override
-    public void insertEmployee(EmployeeDto employee) {
+    public void insertEmployee(EmployeeDto employeeDto) {
+        try (Connection conn = JdbcConfig.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(INSERT_INTO_EMPLOYEES)) {
 
+            // Mapeamos a la entidad
+            Employee employee = new Employee(
+                    employeeDto.getName(),
+                    employeeDto.getSalary()
+            );
+
+            // El id no se setea si ya lo hemos definido como autoincremental en la sentencia de creacion de tabla
+            //pstmt.setInt(1, employee.getId());
+            pstmt.setString(1, employee.getName());
+            pstmt.setDouble(2, employee.getSalary());
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
